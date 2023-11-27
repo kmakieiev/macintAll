1cbe13f71919733917483503b5976205fd7e9453
Kyryl Makieiev <kyryl.makieiev@gmail.com>
2023-11-27 14:03:08 +0100

    Initial Commit

---
+++ macintAll/macintAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  macintAllApp.swift
+//  macintAll
+//
+//  Created by Kyryl Makieiev on 27.11.23.
+//
+
+import SwiftUI
+
+@main
+struct macintAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
